f04c5355ce46f039ed92a0e9778e9291842c09e7
lewiswilkins <lewiswilkins@me.com>
2019-02-28 23:01:39 +0000

    Added additional functionality to Migration.

--- Migration.py
+++ Migration.py
@@ -1,9 +1,10 @@
 import ROOT as r
+from array import array
 
 
 class Migration(object):
     def __init__(self, name, config):
-        r.gErrorIgnoreLevel = r.kFatal
+        r.gErrorIgnoreLevel = r.kFatal  # Override ROOT errors
         self.name = name
         self.config = config
         self._check_config(self.config)
@@ -11,6 +12,31 @@ class Migration(object):
         self.reco_tree = self._get_tree(self.reco_file, config["reco_tree_name"])
         self.truth_file = self._get_file(config["truth_file_name"])
         self.truth_tree = self._get_tree(self.truth_file, config["truth_tree_name"])
+        self.reco_hist_template = self._draw_hist(
+            "reco",
+            config["reco_var"],
+            config["reco_weight"],
+            config["reco_cut"],
+            config["binning"],
+            self.reco_tree,
+        )
+        self.truth_hist_template = self._make_TH1D("truth", config["truth_binning"])
+
+    def _draw_hist(self, name, var, weight, cut, binning, tree):
+        hist = self._make_TH1D(name, binning)
+        tree.Draw(var + ">>" + name, weight + "*" + cut)
+        return hist
+
+    def _make_TH1D(self, name, binning):
+        if isinstance(binning, array):
+            return r.TH1D(name, name, len(binning) - 1, binning)
+        else:
+            print(
+                "Migration::Error: {0} binning is defined as {1}. Please use an array.".format(
+                    name, type(binning)
+                )
+            )
+            exit()
 
     def _get_tree(self, file, tree_name):
         tree = file.Get(tree_name)
@@ -41,8 +67,17 @@ class Migration(object):
             "reco_tree_name",
             "truth_file_name",
             "truth_tree_name",
+            "reco_var",
+            "truth_var",
+            "reco_weight",
+            "truth_weight",
+            "reco_cut",
+            "truth_cut",
+            "reco_binning",
+            "truth_binning",
         ]
         missing_keys = False
+
         for key in required_keys:
             try:
                 config[key]
--- test_migration.py
+++ test_migration.py
@@ -1,13 +1,17 @@
 from Migration import Migration
-
+from array import array
 
 config = {
     "name": "test_migration",
     "reco_file_name": "/scratch3/lwilkins/Data/OutputNtuples_2.4.42_Dec18/ljets_ge4jge1b1smt/ttbar_PP8AFII_m170_Dec18_nominal.root",
     "reco_tree_name": "nominal",
-    "truth_file_name": "file",
-    "truth_tree_name": "tree",
+    "truth_file_name": "/scratch3/lwilkins/Data/OutputNtuples_2.4.42_Dec18/ljets_ge4jge1b1smt/ttbar_PP8AFII_m170_Dec18_nominal.root",
+    "truth_tree_name": "nominal",
+    "reco_binning": array("d", [1, 2, 3, 4]),
+    "truth_binning": array("d", [1, 2, 3, 4]),
 }
+
+
 migration = Migration("test_migration", config)
 
-print("hello")
+print("Complete")
